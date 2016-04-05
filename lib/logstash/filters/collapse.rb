893b7537f18a198b584c9e4fb6f2ee079ce4b882
Dale McDiarmid <dalem@elastic.co>
2016-04-05 21:40:11 +0100

    Initial commit

---
+++ lib/logstash/filters/collapse.rb
@@ -0,0 +1,67 @@
+# encoding: utf-8
+require "logstash/filters/base"
+require "logstash/namespace"
+require 'json'
+
+# This collapse filter will take a list of paths.  Each of these paths will be navigated.  Every value for the path will be extracted and added to the target field if multi_valued is true,
+# otherwise only the first value will be extracted.
+# This filter handles both nested lists and maps - iterating as appropriate.
+#
+class LogStash::Filters::Collapse < LogStash::Filters::Base
+
+  # Example:
+  #
+  # filter {
+  #   collapse {
+  #     map_fields => { "[field][sub_field][sub_sub_field]" => "copy_field" }
+  #     multi_valued => true
+  #   }
+  # }
+  #
+
+  config_name "collapse"
+  
+  config :map_fields, :validate => :hash, :required =>true
+  config :multi_valued, :validate => :boolean, :default => true
+
+
+  public
+  def register
+  end
+
+  public
+  def filter(event)
+    @map_fields.each do |src_fields, dest_field|
+      fields = src_fields.split('][')
+      values = read_field(event,fields)
+      unless values.empty?
+        event[dest_field] =values
+      end
+    end
+    # filter_matched should go in the last line of our successful code
+    filter_matched(event)
+  end
+
+  def read_field(item,fields)
+    values=[]
+    fields.each do |field|
+      field=field.gsub('[','').gsub(']','')
+      sub_item = item[field]
+      if !sub_item.nil?
+        if sub_item.is_a?(Array)
+          sub_item.each do |val|
+            values=values+read_field(val,fields[1..-1])
+          end
+        elsif sub_item.is_a?(Hash)
+            values=values+read_field(sub_item,fields[1..-1])
+        else
+            values.push(sub_item)
+        end
+      else
+        return values
+      end
+    end
+    return values
+  end
+
+end
---
+++ spec/filters/collapse_spec.rb
@@ -0,0 +1,70 @@
+# encoding: utf-8
+require 'spec_helper'
+require "logstash/filters/collapse"
+
+describe LogStash::Filters::Collapse do
+  describe "Test Map Fields" do
+    let(:config) do <<-CONFIG
+      filter {
+        collapse {
+          map_fields => { "[level_a][level_b][level_c]" => "level_field" "[level_a][level_d][level_e]" => "another_field"}
+          multi_valued => false
+        }
+      }
+    CONFIG
+    end
+
+    sample("level_a" => {"level_b" => {"level_c" => "valueA"},"level_d" => {"level_e" => "valueB"}}) do
+      expect(subject).to include("level_field")
+      expect(subject['level_field']).to eq(['valueA'])
+      expect(subject).to include("another_field")
+      expect(subject['another_field']).to eq(['valueB'])
+    end
+  end
+
+
+  describe "Test List Fields" do
+    let(:config) do <<-CONFIG
+      filter {
+        collapse {
+          map_fields => { "[level_a][level_b][level_c]" => "level_field" "[level_a][level_d][level_e]" => "another_field"}
+          multi_valued => false
+        }
+      }
+    CONFIG
+    end
+
+    sample("level_a" => {"level_b" => [{"level_c" => "valueA"},{"level_c" => "valueB"}],"level_d" => [{"level_e" => "valueB"}]}) do
+      expect(subject).to include("level_field")
+      expect(subject['level_field']).to eq(['valueA','valueB'])
+      expect(subject).to include("another_field")
+      expect(subject['another_field']).to eq(['valueB'])
+    end
+
+    sample("level_a" => [{"level_b" => [{"level_c" => "valueA"},{"level_c" => "valueB"}],"level_d" => [{"level_e" => "valueB"}]},{"level_b" => [{"level_c" => "valueA"},{"level_c" => "valueB"}],"level_d" => [{"level_e" => "valueB"}]}]) do
+      expect(subject).to include("level_field")
+      expect(subject['level_field']).to eq(['valueA','valueB','valueA','valueB'])
+      expect(subject).to include("another_field")
+      expect(subject['another_field']).to eq(['valueB','valueB'])
+    end
+  end
+
+  describe "Test Doesn't Exist" do
+    let(:config) do <<-CONFIG
+      filter {
+        collapse {
+          map_fields => { "[level_a][level_b]" => "level_field" "[level_a][level_d][level_e]" => "another_field"}
+          multi_valued => false
+        }
+      }
+    CONFIG
+    end
+
+    sample("level_a" => {"level_b" => [{"level_c" => "valueA"},{"level_c" => "valueB"}],"level_d" => [{"level_e" => "valueB"}]}) do
+      expect(subject).not_to include("level_field")
+      expect(subject).to include("another_field")
+      expect(subject['another_field']).to eq(['valueB'])
+    end
+
+  end
+end
